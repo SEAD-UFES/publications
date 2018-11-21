e8b0cca419470e9a99d845e7d0864210f165a39f
edo9k <edox86@gmail.com>
2018-11-21 15:14:48 -0200

    Adding States to application
    Copied files from old repository and checked to see whether everything
    was still working. The State model still needs to be changed when Cities
    are added (to represent the relationship between cities and states).

---
+++ app/models/state.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const uuid = require('uuid/v4');
+const bcrypt = require('bcrypt');
+const apiRoutes = require('../../config/apiRoutes.json');
+
+module.exports = (sequelize, DataTypes) => {
+  const State = sequelize.define('State', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: DataTypes.STRING,
+    abbreviation: DataTypes.STRING(2),
+    createdAt: {
+      type: DataTypes.DATE(3),
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+    updatedAt: {
+      type: DataTypes.DATE(3),
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    }
+  }, {});
+
+  /*
+  State.associate = function(models) {
+    State.hasMany(models.City, {
+      sourceKey: 'id',
+      foreignKey: 'state_id'
+    });
+    return State;
+  };
+  */
+
+  return State;
+};
---
+++ db/migrations/20180925192016-create-state.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('States', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      abbreviation: {
+        type: Sequelize.STRING(2)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('States');
+  }
+};
