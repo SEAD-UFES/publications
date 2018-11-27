37b3ca55a4bcc2565b27e756843dea29748377d2
edo9k <edox86@gmail.com>
2018-11-27 13:52:13 -0200

    Finishing fyn for cities
    Added follow-your-nose links for cities and used sequelize include to
    load state information together with the cities. State models also have
    their own fyn.

--- app/api/cities.js
+++ app/api/cities.js
@@ -8,27 +8,41 @@ module.exports = app => {
     if (req.params.state.length !== 2){
       res.status(500).json(error.parse('cities-04', null));
     } else {
+     
       models.State
         .findAll({where: {abbreviation:req.params.state}})
         .then(state => {
           if (state !== undefined && state.length > 0) {
+
             models.City
-              .findAll({where: {state_id: state[0].id}})
-              .then(cities => {
-                res.json(cities);
+              .findAll({where: {
+                state_id: state[0].id}, 
+                include: [models.State], 
+                attributes: { exclude: ["state_id"] }
+              })            
+              .then(cities => {                     
+                  res.json(cities);               
               }, e => {
                 res.status(500).json(error.parse('cities-01', e));
-              });      
+              });  
           } else res.status(500).json(error.parse('cities-04', null));
+
       }, e => {
         res.status(500).json(error.parse('cities-03', e));
-      });
+      }); 
+  
+
     }
   }
 
   api.specific = (req, res) => {
     models.City
-      .findById(req.params.id)
+      .findOne({
+        where: {id: req.params.id}, 
+        include: [models.State],
+        attributes: { exclude: ["state_id"] }
+      })
+      //.findById(req.params.id)
       .then(city => {
         res.json(city);
       }, e => {
