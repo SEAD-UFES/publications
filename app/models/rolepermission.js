6044ab5243e44e410700d6258441a199dca00973
Wagner de Andrade Perin <wagnerperin@gmail.com>
2019-02-14 18:54:10 -0200

    Resolving rolePermission error to post id

--- app/models/rolepermission.js
+++ app/models/rolepermission.js
@@ -1,15 +1,23 @@
-'use strict';
 const uuid = require('uuid/v4');
+'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const RolePermission = sequelize.define('RolePermission', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true
+    },
     roleType_id: DataTypes.UUID,
     permission_id: DataTypes.UUID
   }, {});
   RolePermission.associate = function(models) {
     RolePermission.belongsTo(models.Permission, { foreignKey: 'permission_id' });
+
+    return RolePermission;
   };
   RolePermission.beforeCreate((rolePermission, _ ) => {
     rolePermission.id = uuid();
+
     return rolePermission;
   });
   return RolePermission;
