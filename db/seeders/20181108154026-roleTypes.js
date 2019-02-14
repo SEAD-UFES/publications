79f67e69ba7d09c19307e9f2568d6d9a9c6996de
Wagner de Andrade Perin <wagnerperin@gmail.com>
2019-02-14 15:20:40 -0200

    Correcting roleTypes seeder

--- db/seeders/20181108154026-roleTypes.js
+++ db/seeders/20181108154026-roleTypes.js
@@ -5,16 +5,11 @@ const uuid = require('uuid/v4');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('RoleTypes', [
-      { id: uuid(), name: 'Administrador',          description: 'Administrador do sistema.'},
-      { id: uuid(), name: 'Secretário do Curso',    description: 'Recebe demandas dos alunos, professores e tutores.' }, 
-      { id: uuid(), name: 'Professor',              description: 'Cria conteúdos das salas.' }, 
-      { id: uuid(), name: 'Designer Instrucional',  description: '[Inserir descrição]' }, 
-      { id: uuid(), name: 'Editor',                 description: '[Inserir descrição]' }, 
-      { id: uuid(), name: 'Tutor à Distância',      description: 'Atende e avalia os alunos, fica na SEAD.' }, 
-      { id: uuid(), name: 'Tutor Presencial',       description: 'Atende e avalia os alunos em cada polo.' }, 
-      { id: uuid(), name: 'Coordenador de Tutoria', description: '[Inserir descrição]' }, 
-      { id: uuid(), name: 'Candidato',              description: 'Inscrito em processo seletivo, interessado em ser aluno, professor ou tutor.' }, 
-      { id: uuid(), name: 'Aluno',                  description: 'Estudante de Curso EAD da SEAD.' }
+      { id: uuid(), name: 'Administrador', description: 'Administrador do Sistema.'},
+      { id: uuid(), name: 'Gerente',       description: 'Pessoal da Administração da SEAD.' }, 
+      { id: uuid(), name: 'Coordenador',   description: 'Coordena cursos e gerencia seus participantes.' }, 
+      { id: uuid(), name: 'Avaliador',     description: 'Realiza avaliações dos participantes.' }, 
+      { id: uuid(), name: 'Publicador',    description: 'Publica comunicados, editais e resultados.' }
     ], {});
   },
 
