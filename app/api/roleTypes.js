bb7704803ea4ebf04028c908d9048e190e1bbca1
annecchini <fernando.void@gmail.com>
2019-03-07 15:41:42 -0300

    add include permissions on roletypes

--- app/api/roleTypes.js
+++ app/api/roleTypes.js
@@ -1,41 +1,56 @@
 module.exports = app => {
-    const models = require('../models');
-    const api = {};
-    const error = app.errors.roleTypes;
-  
-    api.create = (req, res) => {
-        if (!(Object.prototype.toString.call(req.body) === '[object Object]') || !(req.body.name) || !(req.body.description)) {
-            res.status(400).json(error.parse('roleTypes-01', {}));
-        } else {
-            models.RoleType
-                .create(req.body)
-                .then(_ => {
-                    res.sendStatus(201)
-                }, e => {
-                    res.status(500).json(error.parse('roleTypes-02', e));
-                });
+  const models = require("../models");
+  const api = {};
+  const error = app.errors.roleTypes;
+
+  api.create = (req, res) => {
+    if (
+      !(Object.prototype.toString.call(req.body) === "[object Object]") ||
+      !req.body.name ||
+      !req.body.description
+    ) {
+      res.status(400).json(error.parse("roleTypes-01", {}));
+    } else {
+      models.RoleType.create(req.body).then(
+        _ => {
+          res.sendStatus(201);
+        },
+        e => {
+          res.status(500).json(error.parse("roleTypes-02", e));
         }
-    };
-
-    api.list = (req, res) => {
-        models.RoleType
-            .findAll({})
-            .then(roleTypes => {
-                res.json(roleTypes);
-            }, e => {
-                res.status(500).json(error.parse('roleTypes-02', e));
-            });
+      );
     }
+  };
 
-    api.specific = (req, res) => {
-        models.RoleType
-          .findById(req.params.id)
-          .then(roleType => {
-            res.json(roleType)
-          }, e => {
-            res.status(500).json(error.parse('roleTypes-02', e));
-          });
-      };
-  
-    return api;
-  }
+  api.list = (req, res) => {
+    models.RoleType.findAll({}).then(
+      roleTypes => {
+        res.json(roleTypes);
+      },
+      e => {
+        res.status(500).json(error.parse("roleTypes-02", e));
+      }
+    );
+  };
+
+  api.specific = (req, res) => {
+    models.RoleType.find({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: models.Permission,
+          required: false
+        }
+      ]
+    }).then(
+      roleType => {
+        return res.json(roleType);
+      },
+      e => {
+        res.status(500).json(error.parse("roleTypes-02", e));
+      }
+    );
+  };
+
+  return api;
+};
