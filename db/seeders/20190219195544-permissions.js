b565d27b47c3b753871aab4ed25f207da8e1a8a1
edo9k <edox86@gmail.com>
2019-03-19 16:54:08 -0300

    Reworded Permission descriptions
    Now starting with the model name, then action.
    
    Reworded role specific permissions
    
    Reworded Permission descriptions
    
    Now starting with the model name, then action.
    
    Reworded role specific permissions
    
    Corrected order and description typo

--- db/seeders/20190219195544-permissions.js
+++ db/seeders/20190219195544-permissions.js
@@ -25,139 +25,25 @@ module.exports = {
           target_ids[el.urn] = el.id;
         }
 
-        return queryInterface.bulkInsert('Permissions', [{
-            id: uuid(),
-            name: 'Listar usuários.',
-            description: 'descrição',
-            action_id: action_ids['GET'],
-            target_id: target_ids['/v1/users']
-          },
-          {
-            id: uuid(),
-            name: 'Criar usuário.',
-            description: 'descrição',
-            action_id: action_ids['POST'],
-            target_id: target_ids['/v1/users']
-          },
-          {
-            id: uuid(),
-            name: 'Editar usuário.',
-            description: 'descrição',
-            action_id: action_ids['PUT'],
-            target_id: target_ids['/v1/users/:id']
-          },
-          {
-            id: uuid(),
-            name: 'Acessar usuário.',
-            description: 'descrição',
-            action_id: action_ids['GET'],
-            target_id: target_ids['/v1/users/:id']
-          },
-
-          {
-            id: uuid(),
-            name: 'Criar chamada.',
-            description: 'descrição',
-            action_id: action_ids['POST'],
-            target_id: target_ids['/v1/calls']
-          },
-          {
-            id: uuid(),
-            name: 'Acessar chamada.',
-            description: 'descrição',
-            action_id: action_ids['GET'],
-            target_id: target_ids['/v1/calls/:id']
-          },
-          {
-            id: uuid(),
-            name: 'Editar chamada.',
-            description: 'descrição',
-            action_id: action_ids['PUT'],
-            target_id: target_ids['/v1/calls/:id']
-          },
-          {
-            id: uuid(),
-            name: 'Apagar chamada.',
-            description: 'descrição',
-            action_id: action_ids['DELETE'],
-            target_id: target_ids['/v1/calls/:id']
-          },
-
-          {
-            id: uuid(),
-            name: 'Criar curso.',
-            description: 'descrição',
-            action_id: action_ids['POST'],
-            target_id: target_ids['/v1/courses']
-          },
-
-          {
-            id: uuid(),
-            name: 'Acessar pessoa.',
-            description: 'descrição',
-            action_id: action_ids['GET'],
-            target_id: target_ids['/v1/people/:id']
-          },
-          {
-            id: uuid(),
-            name: 'Editar pessoa.',
-            description: 'descrição',
-            action_id: action_ids['PUT'],
-            target_id: target_ids['/v1/people/:id']
-          },
-
-          {
-            id: uuid(),
-            name: 'Editar etapa.',
-            description: 'descrição',
-            action_id: action_ids['PUT'],
-            target_id: target_ids['/v1/steps']
-          },
-          {
-            id: uuid(),
-            name: 'Criar etapa.',
-            description: 'descrição',
-            action_id: action_ids['POST'],
-            target_id: target_ids['/v1/steps']
-          },
-
-          {
-            id: uuid(),
-            name: 'Criar vaga.',
-            description: 'descrição',
-            action_id: action_ids['POST'],
-            target_id: target_ids['/v1/vacancies']
-          },
-          {
-            id: uuid(),
-            name: 'Editar vaga.',
-            description: 'descrição',
-            action_id: action_ids['PUT'],
-            target_id: target_ids['/v1/vacancies/:id']
-          },
-          {
-            id: uuid(),
-            name: 'Acessar vaga.',
-            description: 'descrição',
-            action_id: action_ids['GET'],
-            target_id: target_ids['/v1/vacancies/:id']
-          },
-
-          {
-            id: uuid(),
-            name: 'Criar processo seletivo.',
-            description: 'descrição',
-            action_id: action_ids['POST'],
-            target_id: target_ids['/v1/selectiveprocesses']
-          },
-          {
-            id: uuid(),
-            name: 'Editar processo seletivo.',
-            description: 'descrição',
-            action_id: action_ids['PUT'],
-            target_id: target_ids['/v1/selectiveprocesses/:id']
-          }
-
+        return queryInterface.bulkInsert('Permissions', [
+          { id: uuid(), name: 'usuários listar', description: 'descrição', action_id: action_ids['GET'], target_id: target_ids['/v1/users'] },
+          { id: uuid(), name: 'usuário criar', description: 'descrição', action_id: action_ids['POST'], target_id: target_ids['/v1/users'] },
+          { id: uuid(), name: 'usuário editar', description: 'descrição', action_id: action_ids['PUT'], target_id: target_ids['/v1/users/:id'] },
+          { id: uuid(), name: 'usuário acessar', description: 'descrição', action_id: action_ids['GET'], target_id: target_ids['/v1/users/:id'] },
+          { id: uuid(), name: 'chamada criar', description: 'descrição', action_id: action_ids['POST'], target_id: target_ids['/v1/calls']  },
+          { id: uuid(), name: 'chamada acessar', description: 'descrição', action_id: action_ids['GET'], target_id: target_ids['/v1/calls/:id'] },
+          { id: uuid(), name: 'chamada editar', description: 'descrição', action_id: action_ids['PUT'], target_id: target_ids['/v1/calls/:id'] },
+          { id: uuid(), name: 'chamada apagar', description: 'descrição', action_id: action_ids['DELETE'], target_id: target_ids['/v1/calls/:id'] },
+          { id: uuid(), name: 'curso criar', description: 'descrição', action_id: action_ids['POST'], target_id: target_ids['/v1/courses'] },
+          { id: uuid(), name: 'pessoa acessar', description: 'descrição', action_id: action_ids['GET'], target_id: target_ids['/v1/people/:id'] },
+          { id: uuid(), name: 'pessoa editar', description: 'descrição', action_id: action_ids['PUT'], target_id: target_ids['/v1/people/:id'] },
+          { id: uuid(), name: 'etapa editar', description: 'descrição', action_id: action_ids['PUT'], target_id: target_ids['/v1/steps'] },
+          { id: uuid(), name: 'etapa criar', description: 'descrição', action_id: action_ids['POST'], target_id: target_ids['/v1/steps'] },
+          { id: uuid(), name: 'vaga criar', description: 'descrição', action_id: action_ids['POST'], target_id: target_ids['/v1/vacancies'] },
+          { id: uuid(), name: 'vaga editar', description: 'descrição', action_id: action_ids['PUT'], target_id: target_ids['/v1/vacancies/:id'] },
+          { id: uuid(), name: 'vaga acessar', description: 'descrição', action_id: action_ids['GET'], target_id: target_ids['/v1/vacancies/:id'] },
+          { id: uuid(), name: 'processo seletivo criar', description: 'descrição', action_id: action_ids['POST'], target_id: target_ids['/v1/selectiveprocesses'] },
+          { id: uuid(), name: 'processo seletivo editar', description: 'descrição', action_id: action_ids['PUT'], target_id: target_ids['/v1/selectiveprocesses/:id'] }
         ], {});
 
       });
