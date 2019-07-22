286d9c41a10e249cb44f69ce07b06069bea1c0e4
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-07-22 15:11:29 -0300

    little cors_middleware added

--- config/express.js
+++ config/express.js
@@ -1,31 +1,36 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const consign = require('consign');
-const secrets = require('./secrets.json');
-const apiRoutes = require('./apiRoutes.json');
+const bodyParser = require("body-parser");
+const consign = require("consign");
+const secrets = require("./secrets.json");
+const apiRoutes = require("./apiRoutes.json");
 
-app.use(bodyParser.json());
+const cors_middleware = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  next();
+};
+
+app.use(cors_middleware, bodyParser.json());
+
 //Load Secrets File.
 secrets.forEach(s => {
-    app.set(s.key, s.value);
+  app.set(s.key, s.value);
 });
 //Load API Routes file
 apiRoutes.forEach(r => {
-    app.set(r.key, r.value);
+  app.set(r.key, r.value);
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-
-
-consign({cwd: 'app'})
-    .include('helpers')
-    .then('errors')
-    .then('models/index.js')
-    .then('api')
-    .then('routes')
-    .then('swagger')
-    .into(app);
+consign({ cwd: "app" })
+  .include("helpers")
+  .then("errors")
+  .then("models/index.js")
+  .then("api")
+  .then("routes")
+  .then("swagger")
+  .into(app);
 
 module.exports = app;
