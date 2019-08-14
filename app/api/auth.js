f4c2cd756ac6ce2c0c8cd852b02026a2e58772b2
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-08-14 14:01:16 -0300

    modifiy checkAccessLevel auth api to get permissions and manage globalRoles

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,189 +1,221 @@
 module.exports = app => {
+  const models = require('../models')
+  const jwt = require('jsonwebtoken')
+  const api = {}
+  const error = app.errors.auth
+  const { bodyRoute, paramRoute } = require('../helpers/courseInfo')
+  const { getPermission } = require('../helpers/permissionInfo')
+  const { isAdmin, hasGlobalPermission } = require('../helpers/permissionCheck')
 
-    const models = require('../models');
-    const jwt = require('jsonwebtoken');
-    const api = {};
-    const error = app.errors.auth;
-    const { bodyRoute, paramRoute } = require('../helpers/courseInfo');
-
-    api.authenticate = (req, res) => {
-        if(Array.isArray(req.body) || (req.body.constructor === Object && Object.keys(req.body).length === 0) || !req.body.login || !req.body.password) res.status(400).json(error.parse('auth-03', new Error('This resource spect a JSON user object in the body request containing username and password.')));
-        else{
-            models.User.findOne({where: {login: req.body.login}})
-                .then(user => {
-                    if(!user){
-                        res.status(401).json(error.parse('auth-04', new Error('This login was not found.')));
-                    }else if(!user.authorized) res.status(401).json(error.parse('auth-09', new Error("User is unauthorized")));
-                          else user.validPassword(req.body.password)
-                                .then(valid => {
-                                    if(valid){
-                                        let access_token = jwt.sign({ data: user.id }, app.get('jwt_secret'), { expiresIn: "6h" });
-                                        res.json({access_token, userMessage: 'Authentication success'});
-                                    }else{
-                                        res.status(401).json(error.parse('auth-05', new Error('Wrong password.')));
-                                    }
-                                });
-                    
-                }, e => res.status(500).json(error.parse('auth-06', new Error('Internal server error.'))));
-        }
-    };
-
-    api.authenticationRequired = (req, res, next) => {
-        try{
-            decoded = jwt.verify(req.headers['x-access-token'], app.get('jwt_secret'));
-            models.User.findById(
-                decoded.data, 
-                {
-                    include: [{
-                        model: models.UserRole,
-                        required: false,
-                        attributes: {
-                            exclude: ['roleType_id', 'user_id', 'course_id']
-                        },
-                        include: [
-                            {
-                                model: models.RoleType,
-                              required: false,
-                              include: [
-                                {
-                                  model: models.Permission,
-                                  required: false
-                                }
-                              ]
-                            },
-                            {
-                                model: models.Course,
-                                required: false
-                            }
-                        ]
-                    }]
-                })
-                .then(user => {
-                    req.user = user;
-                    next();
-                }, e => {
-                    res.status(500).json(error.parse('auth-03', e));
-                });
-        }catch(e){
-            switch(e.name){
-                case "TokenExpiredError": 
-                    res.status(401).json(error.parse('auth-01', e));
-                    break;
-                case "JsonWebTokenError":
-                    res.status(401).json(error.parse('auth-02', e));
-                    break;
-                default:
-                    res.status(401).json(error.parse('auth-07', e));
-            }
-        }
+  api.authenticate = (req, res) => {
+    if (
+      Array.isArray(req.body) ||
+      (req.body.constructor === Object && Object.keys(req.body).length === 0) ||
+      !req.body.login ||
+      !req.body.password
+    )
+      res
+        .status(400)
+        .json(
+          error.parse(
+            'auth-03',
+            new Error('This resource spect a JSON user object in the body request containing username and password.')
+          )
+        )
+    else {
+      models.User.findOne({ where: { login: req.body.login } }).then(
+        user => {
+          if (!user) {
+            res.status(401).json(error.parse('auth-04', new Error('This login was not found.')))
+          } else if (!user.authorized) res.status(401).json(error.parse('auth-09', new Error('User is unauthorized')))
+          else
+            user.validPassword(req.body.password).then(valid => {
+              if (valid) {
+                let access_token = jwt.sign({ data: user.id }, app.get('jwt_secret'), { expiresIn: '6h' })
+                res.json({ access_token, userMessage: 'Authentication success' })
+              } else {
+                res.status(401).json(error.parse('auth-05', new Error('Wrong password.')))
+              }
+            })
+        },
+        e => res.status(500).json(error.parse('auth-06', new Error('Internal server error.')))
+      )
     }
+  }
 
-    api.adminRequired = (req, res, next) => {
-        if(req.user.UserRoles.some(o => o.RoleType.name == 'Administrador')) next();
-        else res.status(401).json(error.parse('auth-08', new Error("Administrator level required")));
-    }
-
-    api.checkAccessLevel = (req, res, next) => {
-        function callBack(finded){
-            if(finded) next();
-            else res.status(401).json(error.parse('auth-10', new Error("You don't have permission to require this operation")));
-        }
-
-        let processed = 0;
-        let finded = false;
-        if(req.user.UserRoles.length === 0) callBack(false);
-        else if(req.user.UserRoles.some(o => o.RoleType.name == 'Administrador')) next();
-        else req.user.UserRoles.forEach((role, i, arr) => {
-               models.RolePermission.findAll({
-                where: {
-                    roleType_id: role.RoleType.id
-                },
+  api.authenticationRequired = (req, res, next) => {
+    try {
+      decoded = jwt.verify(req.headers['x-access-token'], app.get('jwt_secret'))
+      models.User.findById(decoded.data, {
+        include: [
+          {
+            model: models.UserRole,
+            required: false,
+            attributes: {
+              exclude: ['roleType_id', 'user_id', 'course_id']
+            },
+            include: [
+              {
+                model: models.RoleType,
+                required: false,
                 include: [
-                    {
-                        model: models.Permission,
-                        required: false,
-                        include: [
-                            {
-                                model: models.Action,
-                                required: false,
-                                attributes: ['name']
-                            },
-                            {
-                                model: models.Target,
-                                required: false,
-                                attributes: ['name', 'urn']
-                            }
-                        ],
-                        attributes: ['action_id', 'target_id']
-                    }
-                ],
-                attributes: ['permission_id']
-                }).then(rolePermissions => {
-                    processed++;
-                    console.log(req.route.path);
-                    if(rolePermissions.some(rp => rp.Permission.Action.name == req.method && (rp.Permission.Target.urn == req.originalUrl || rp.Permission.Target.urn == req.route.path)) ) {
-                        finded = true;
-                    }
-                    if(processed === arr.length)
-                        if(finded) callBack(true);
-                        else callBack(false);
-                    else if(finded) callBack(true);
-                });
-        });
+                  {
+                    model: models.Permission,
+                    required: false
+                  }
+                ]
+              },
+              {
+                model: models.Course,
+                required: false
+              }
+            ]
+          }
+        ]
+      }).then(
+        user => {
+          req.user = user
+          next()
+        },
+        e => {
+          res.status(500).json(error.parse('auth-03', e))
+        }
+      )
+    } catch (e) {
+      switch (e.name) {
+        case 'TokenExpiredError':
+          res.status(401).json(error.parse('auth-01', e))
+          break
+        case 'JsonWebTokenError':
+          res.status(401).json(error.parse('auth-02', e))
+          break
+        default:
+          res.status(401).json(error.parse('auth-07', e))
+      }
+    }
+  }
+
+  api.adminRequired = (req, res, next) => {
+    if (req.user.UserRoles.some(o => o.RoleType.name == 'Administrador')) next()
+    else res.status(401).json(error.parse('auth-08', new Error('Administrator level required')))
+  }
+
+  api.checkAccessLevel = (req, res, next) => {
+    function callBack(finded) {
+      if (finded) next()
+      else
+        res.status(401).json(error.parse('auth-10', new Error("You don't have permission to require this operation")))
     }
 
-    api.checkCourseStaff = async (req, res, next) => {
-      if(req.user.UserRoles && req.user.UserRoles.length === 0) {
-        res.status(500).json(error.parse('auth-11', 'This user has no Permissions.'));
-      } else {
-
-        const isAdmin = req.user.UserRoles
-          .map(x => x.RoleType.name)
-          .includes('Administrador')
-
-        if (isAdmin) {
-          next()
-        } else {
-
-          let allowedCourse
-          let targetCourse
-
-          console.log(req.method)
-
-          if (req.method === 'GET' || req.method === 'DELETE' || req.method === 'PUT') {
-            try {
-              targetCourse = await paramRoute(req.url);
-            } catch (e) {
-              res.status(500).json(error.parse('auth-11', e));
+    let processed = 0
+    let finded = false
+    if (req.user.UserRoles.length === 0) callBack(false)
+    else if (req.user.UserRoles.some(o => o.RoleType.name == 'Administrador')) next()
+    else
+      req.user.UserRoles.forEach((role, i, arr) => {
+        models.RolePermission.findAll({
+          where: {
+            roleType_id: role.RoleType.id
+          },
+          include: [
+            {
+              model: models.Permission,
+              required: false,
+              include: [
+                {
+                  model: models.Action,
+                  required: false,
+                  attributes: ['name']
+                },
+                {
+                  model: models.Target,
+                  required: false,
+                  attributes: ['name', 'urn']
+                }
+              ],
+              attributes: ['action_id', 'target_id']
             }
-          } else if (req.body.course_id || req.query.course_id) {
-            targetCourse = (req.body.course_id) ? req.body.course_id : req.query.course_id; 
-          } else {
-            try {
-              targetCourse = (req.body) ? await bodyRoute(req.body) : await bodyRoute(req.query)
-            } catch (e) {
-              res.status(500).json(error.parse('auth-11', e));
-            }
+          ],
+          attributes: ['permission_id']
+        }).then(rolePermissions => {
+          processed++
+          console.log(req.route.path)
+          if (
+            rolePermissions.some(
+              rp =>
+                rp.Permission.Action.name == req.method &&
+                (rp.Permission.Target.urn == req.originalUrl || rp.Permission.Target.urn == req.route.path)
+            )
+          ) {
+            finded = true
           }
+          if (processed === arr.length)
+            if (finded) callBack(true)
+            else callBack(false)
+          else if (finded) callBack(true)
+        })
+      })
+  }
 
-          console.log(`targetCourse ${targetCourse}`)
+  api.checkCourseStaff = async (req, res, next) => {
+    if (req.user.UserRoles && req.user.UserRoles.length === 0) {
+      res.status(500).json(error.parse('auth-11', 'This user has no Permissions.'))
+    } else {
+      //É administrador
+      //const isAdmin = req.user.UserRoles.map(x => x.RoleType.name).includes('Administrador')
+      const Admin = isAdmin(req.user)
+      if (Admin) {
+        next()
+      }
 
-          if (targetCourse) {
-            allowedCourse = req.user.UserRoles
-              .filter(x => x.Course)
-              .map(x => x.Course.id)
-              .includes(targetCourse)
+      //Tem papel global com as permissão
+      const needed_permission = getPermission({ url: req.url, method: req.method })
+      const GlobalPermission = hasGlobalPermission(req.user, needed_permission)
+      if (GlobalPermission) {
+        next()
+      }
 
-            if (allowedCourse) {
-              next()
-            } else {
-              res.status(500).json(error.parse('auth-11', 'This user does not have the required permissions to access this resource.'));
-            }
+      //Outros casos
+      else {
+        let allowedCourse
+        let targetCourse
+
+        if (req.method === 'GET' || req.method === 'DELETE' || req.method === 'PUT') {
+          try {
+            targetCourse = await paramRoute(req.url)
+          } catch (e) {
+            res.status(500).json(error.parse('auth-11', e))
+          }
+        } else if (req.body.course_id || req.query.course_id) {
+          targetCourse = req.body.course_id ? req.body.course_id : req.query.course_id
+        } else {
+          try {
+            targetCourse = req.body ? await bodyRoute(req.body) : await bodyRoute(req.query)
+          } catch (e) {
+            res.status(500).json(error.parse('auth-11', e))
+          }
+        }
+
+        if (targetCourse) {
+          allowedCourse = req.user.UserRoles.filter(x => x.Course)
+            .map(x => x.Course.id)
+            .includes(targetCourse)
+
+          console.log('\n', `targetCourse:${targetCourse}`, '\n')
+
+          if (allowedCourse) {
+            next()
+          } else {
+            console.log('\n', `allowedCourse:${allowedCourse}`, '\n')
+
+            res
+              .status(500)
+              .json(error.parse('auth-11', 'This user does not have the required permissions to access this resource.'))
           }
         }
       }
     }
+  }
 
-    return api;
+  return api
 }
-
---
+++ app/helpers/permissionInfo.js
@@ -0,0 +1,164 @@
+const models = require('../models')
+
+const getPermission = options => {
+  if (!options.url) throw new Error('url option needed.')
+  if (!options.method) throw new Error('method option needed.')
+
+  const params = options.url.split('/')
+  const id = params.pop()
+  const model = params.pop()
+  const method = options.method
+
+  permissions = {
+    //selective process
+    process_create: 'processo seletivo criar',
+    process_read: 'processo seletivo listar',
+    process_update: 'processo seletivo editar',
+    process_delete: 'processo seletivo apagar',
+    process_list: 'processo seletivo listar',
+    //publications
+    publication_create: 'publication_create',
+    publication_read: 'publication_read',
+    publication_update: 'publication_update',
+    publication_delete: 'publication_delete',
+    publication_list: '',
+    //calls
+    call_create: 'chamada criar',
+    call_read: 'chamada acessar',
+    call_update: 'chamada editar',
+    call_delete: 'chamada apagar',
+    call_list: '',
+    //steps
+    step_create: 'etapa criar',
+    step_read: 'etapa acessar',
+    step_update: 'etapa editar',
+    step_delete: 'etapa apagar',
+    step_list: '',
+    //vacancies
+    vacancy_create: 'vaga criar',
+    vacancy_read: 'vaga acessar',
+    vacancy_update: 'vaga editar',
+    vacancy_delete: 'vaga apagar',
+    vacancy_list: ''
+  }
+
+  switch (model) {
+    case 'selectiveprocesses':
+      switch (method) {
+        case 'POST':
+          return permissions.process_create
+          break
+        case 'GET':
+          if (!id) {
+            return permissions.process_list
+          } else {
+            return permissions.process_read
+          }
+          break
+        case 'PUT':
+          return permissions.process_update
+          break
+        case 'DELETE':
+          return permissions.process_delete
+          break
+        default:
+          throw new Error('Unable to find any method related to this route.')
+      }
+      break
+    case 'publications':
+      switch (method) {
+        case 'POST':
+          return permissions.publication_create
+          break
+        case 'GET':
+          if (!id) {
+            return permissions.publication_list
+          } else {
+            return permissions.publication_read
+          }
+          break
+        case 'PUT':
+          return permissions.publication_update
+          break
+        case 'DELETE':
+          return permissions.publication_delete
+          break
+        default:
+          throw new Error('Unable to find any method related to this route.')
+      }
+      break
+
+    case 'calls':
+      switch (method) {
+        case 'POST':
+          return permissions.call_create
+          break
+        case 'GET':
+          if (!id) {
+            return permissions.call_list
+          } else {
+            return permissions.call_read
+          }
+          break
+        case 'PUT':
+          return permissions.call_update
+          break
+        case 'DELETE':
+          return permissions.call_delete
+          break
+        default:
+          throw new Error('Unable to find any method related to this route.')
+      }
+      break
+
+    case 'steps':
+      switch (method) {
+        case 'POST':
+          return permissions.step_create.break
+        case 'GET':
+          if (!id) {
+            return permissions.step_list
+          } else {
+            return permissions.step_read
+          }
+          break
+        case 'PUT':
+          return permissions.step_update
+          break
+        case 'DELETE':
+          return permissions.step_delete
+          break
+        default:
+          throw new Error('Unable to find any method related to this route.')
+      }
+      break
+
+    case 'vacancies':
+      switch (method) {
+        case 'POST':
+          return permissions.vacancy_create
+          break
+        case 'GET':
+          if (!id) {
+            return permissions.vacancy_list
+          } else {
+            return permissions.vacancy_read
+          }
+          break
+        case 'PUT':
+          return permissions.vacancy_update
+          break
+        case 'DELETE':
+          return permissions.vacancy_delete
+          break
+        default:
+          throw new Error('Unable to find any method related to this route.')
+      }
+      break
+
+    default:
+      throw new Error('Unable to find any model related to this route.')
+  }
+}
+
+module.exports = { getPermission }
