ed4c484c062af8aeb25454edb5d81dc8811ceea8
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-08-15 13:51:39 -0300

    remove adminRequired from getList assignemts routes

--- app/routes/assignments.js
+++ app/routes/assignments.js
@@ -1,14 +1,14 @@
 module.exports = app => {
-    const api = app.api.assignments;
-    const authApi = app.api.auth;
+  const api = app.api.assignments
+  const authApi = app.api.auth
 
-    app.route(app.get('assignmentApiRoute'))
-        .post(authApi.authenticationRequired, authApi.adminRequired, api.create)
-        .get(authApi.authenticationRequired, authApi.adminRequired, api.list);
-    
-    app.route(app.get('assignmentApiRoute')+"/:id")
-        .put(authApi.authenticationRequired, authApi.adminRequired, api.update)
-        .delete(authApi.authenticationRequired, authApi.adminRequired, api.delete);
-        
+  app
+    .route(app.get('assignmentApiRoute'))
+    .post(authApi.authenticationRequired, authApi.adminRequired, api.create)
+    .get(authApi.authenticationRequired, api.list)
+
+  app
+    .route(app.get('assignmentApiRoute') + '/:id')
+    .put(authApi.authenticationRequired, authApi.adminRequired, api.update)
+    .delete(authApi.authenticationRequired, authApi.adminRequired, api.delete)
 }
-  
