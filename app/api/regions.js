e38cad830f05d1f8dc317581cdadf90887df4fad
edo9k <edox86@gmail.com>
2019-09-13 14:59:53 -0300

    Fixed variable in Region
    The variable refered to 'course', which is probably the place the code
    was copied from, corrected it to 'region' and it's working now.

--- app/api/regions.js
+++ app/api/regions.js
@@ -1,47 +1,55 @@
+/** @format */
+
 module.exports = app => {
-    const models = require('../models');
-    const api = {};
-    const error = app.errors.regions;
-  
-    api.create = (req, res) => {
-        if (!(Object.prototype.toString.call(req.body) === '[object Object]') || !(req.body.name) || !(req.body.description)) {
-            res.status(400).json(error.parse('regions-01', {}));
-        } else {
-            models.Region
-                .create(req.body)
-                .then(_ => {
-                    res.sendStatus(201)
-                }, e => {
-                    res.status(500).json(error.parse('regions-02', e));
-                });
+  const models = require('../models')
+  const api = {}
+  const error = app.errors.regions
+
+  api.create = (req, res) => {
+    if (!(Object.prototype.toString.call(req.body) === '[object Object]') || !req.body.name || !req.body.description) {
+      res.status(400).json(error.parse('regions-01', {}))
+    } else {
+      models.Region.create(req.body).then(
+        _ => {
+          res.sendStatus(201)
+        },
+        e => {
+          res.status(500).json(error.parse('regions-02', e))
         }
-    };
-
-    api.list = (req, res) => {
-        models.Region
-            .findAll({})
-            .then(regions => {
-                res.json(regions);
-            }, e => {
-                res.status(500).json(error.parse('regions-02', e));
-            });
+      )
     }
-
-    api.update = (req, res) => {
-        models.Region
-         .findById(req.params.id)
-         .then(region => {
-            if(!region) res.status(400).json(error.parse('regions-03', {}));
-            else course.update(req.body, {fields: Object.keys(req.body)})
-                       .then(updated => res.json(updated), e => res.status(500).json(error.parse('regions-02', e)));
-         }, e => res.status(500).json(error.parse('regions-02', e)));
-    }
-
-    api.delete = (req, res) => {
-        models.Region
-            .destroy({ where: { id: req.params.id } })
-            .then(_ => res.sendStatus(204), e => res.status(500).json(error.parse('regions-02', e)));
-    }
-  
-    return api;
   }
+
+  api.list = (req, res) => {
+    models.Region.findAll({}).then(
+      regions => {
+        res.json(regions)
+      },
+      e => {
+        res.status(500).json(error.parse('regions-02', e))
+      }
+    )
+  }
+
+  api.update = (req, res) => {
+    models.Region.findById(req.params.id).then(
+      region => {
+        if (!region) res.status(400).json(error.parse('regions-03', {}))
+        else
+          region
+            .update(req.body, { fields: Object.keys(req.body) })
+            .then(updated => res.json(updated), e => res.status(500).json(error.parse('regions-02', e)))
+      },
+      e => res.status(500).json(error.parse('regions-02', e))
+    )
+  }
+
+  api.delete = (req, res) => {
+    models.Region.destroy({ where: { id: req.params.id } }).then(
+      _ => res.sendStatus(204),
+      e => res.status(500).json(error.parse('regions-02', e))
+    )
+  }
+
+  return api
+}
