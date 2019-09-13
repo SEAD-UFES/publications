650d6d503b755d7850e9cfecb1ef1bee898248ed
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-09-13 14:18:55 -0300

    add notice actions on seed

--- db/seeders/20190218172610-targets.js
+++ db/seeders/20190218172610-targets.js
@@ -1,34 +1,39 @@
-'use strict';
+'use strict'
 
-const uuid = require('uuid/v4');
+const uuid = require('uuid/v4')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Targets', [
- 
-      { id: uuid(), name: 'user api (general)', urn: '/v1/users' },
-      { id: uuid(), name: 'user api (specific)', urn: '/v1/users/:id' },       
-  
-      { id: uuid(), name: 'call api (general)', urn: '/v1/calls' },
-      { id: uuid(), name: 'call api (specific)', urn: '/v1/calls/:id' },
-   
-      { id: uuid(), name: 'course api (general)', urn: '/v1/courses' },
-    
-      { id: uuid(), name: 'person api (specific)', urn: '/v1/people/:id' },
+    return queryInterface.bulkInsert(
+      'Targets',
+      [
+        { id: uuid(), name: 'user api (general)', urn: '/v1/users' },
+        { id: uuid(), name: 'user api (specific)', urn: '/v1/users/:id' },
 
-      { id: uuid(), name: 'step api (general)', urn: '/v1/steps' },
-      { id: uuid(), name: 'step api (specific)', urn: '/v1/steps/:id' },
+        { id: uuid(), name: 'person api (specific)', urn: '/v1/people/:id' },
 
-      { id: uuid(), name: 'vacancy api (general)', urn: '/v1/vacancies' },
-      { id: uuid(), name: 'vacancy api (specific)', urn: '/v1/vacancies/:id' },
+        { id: uuid(), name: 'course api (general)', urn: '/v1/courses' },
 
-      { id: uuid(), name: 'selective process api (general)', urn: '/v1/selectiveprocesses' },
-      { id: uuid(), name: 'selective process api (specific)', urn: '/v1/selectiveprocesses/:id' },
+        { id: uuid(), name: 'selective process api (general)', urn: '/v1/selectiveprocesses' },
+        { id: uuid(), name: 'selective process api (specific)', urn: '/v1/selectiveprocesses/:id' },
 
-    ], {});
+        { id: uuid(), name: 'call api (general)', urn: '/v1/calls' },
+        { id: uuid(), name: 'call api (specific)', urn: '/v1/calls/:id' },
+
+        { id: uuid(), name: 'notice api (general)', urn: '/v1/notices' },
+        { id: uuid(), name: 'notice api (specific)', urn: '/v1/notices/:id' },
+
+        { id: uuid(), name: 'step api (general)', urn: '/v1/steps' },
+        { id: uuid(), name: 'step api (specific)', urn: '/v1/steps/:id' },
+
+        { id: uuid(), name: 'vacancy api (general)', urn: '/v1/vacancies' },
+        { id: uuid(), name: 'vacancy api (specific)', urn: '/v1/vacancies/:id' }
+      ],
+      {}
+    )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Targets', null, {});
+    return queryInterface.bulkDelete('Targets', null, {})
   }
-};
+}
