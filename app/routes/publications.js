08a6c5ba89e6af598b7c91e93f46a509b5a9808e
Fernando Lyrio Annecchini <fernando.void@gmail.com>
2019-10-24 14:53:43 -0300

    open access to get publication

--- app/routes/publications.js
+++ app/routes/publications.js
@@ -1,17 +1,24 @@
 module.exports = app => {
-  const api = app.api.publications;
-  const fileUpload = app.helpers.fileUpload;
-  const authApi = app.api.auth;
+  const api = app.api.publications
+  const fileUpload = app.helpers.fileUpload
+  const authApi = app.api.auth
 
-  app.route(app.get('publicationApiRoute'))
-    .post(authApi.authenticationRequired, authApi.checkCourseStaff, authApi.checkAccessLevel, fileUpload.upload, fileUpload.handleError, api.create); 
+  app
+    .route(app.get('publicationApiRoute'))
+    .post(
+      authApi.authenticationRequired,
+      authApi.checkCourseStaff,
+      authApi.checkAccessLevel,
+      fileUpload.upload,
+      fileUpload.handleError,
+      api.create
+    )
 
-  app.route(app.get('publicationApiRoute')+"/:id")
-    .get(authApi.authenticationRequired, authApi.checkCourseStaff, authApi.checkAccessLevel, api.specific)
+  app
+    .route(app.get('publicationApiRoute') + '/:id')
+    .get(api.specific)
     .put(authApi.authenticationRequired, authApi.checkCourseStaff, authApi.checkAccessLevel, api.update)
-    .delete(authApi.authenticationRequired, authApi.checkCourseStaff, authApi.checkAccessLevel, api.delete);
+    .delete(authApi.authenticationRequired, authApi.checkCourseStaff, authApi.checkAccessLevel, api.delete)
 
-  app.route(app.get('publicationApiRoute')+'/download/:file')
-    .get(api.download);
+  app.route(app.get('publicationApiRoute') + '/download/:file').get(api.download)
 }
-
