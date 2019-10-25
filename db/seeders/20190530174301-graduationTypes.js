b64becaf3606e1026da4877e8ec7849b147cff9b
edo9k <edox86@gmail.com>
2019-10-25 17:45:36 -0300

    Added categories to GraduationTypes

--- db/seeders/20190530174301-graduationTypes.js
+++ db/seeders/20190530174301-graduationTypes.js
@@ -1,19 +1,28 @@
-'use strict';
+/** @format */
 
-const uuid = require('uuid/v4');
+'use strict'
+
+const uuid = require('uuid/v4')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('GraduationTypes', [
-      { id: uuid(), name: 'Ensino Médio' },
-      { id: uuid(), name: 'Ensino Técnico' },
-      { id: uuid(), name: 'Graduação' },
-      { id: uuid(), name: 'Mestrado' },
-      { id: uuid(), name: 'Doutorado' },
-    ], {});
+    return queryInterface.bulkInsert(
+      'GraduationTypes',
+      [
+        { id: uuid(), name: 'Ensino Médio' },
+        { id: uuid(), name: 'Ensino Técnico' },
+        { id: uuid(), name: 'Graduação' },
+        { id: uuid(), name: 'Aperfeiçoamento' },
+        { id: uuid(), name: 'Especialização' },
+        { id: uuid(), name: 'Mestrado' },
+        { id: uuid(), name: 'Doutorado' },
+        { id: uuid(), name: ' N/A ' }
+      ],
+      {}
+    )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('GraduationTypes', null, {}); 
+    return queryInterface.bulkDelete('GraduationTypes', null, {})
   }
-};
+}
