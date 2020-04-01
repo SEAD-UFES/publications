447df43289006acf8ed37955dcc097304e2cf338
annecchini <fernando.void@gmail.com>
2020-04-01 17:40:34 -0300

    migration ok, seed ok, model todo

---
+++ db/migrations/20200401194642-rebuild-User.js
@@ -0,0 +1,93 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        //id ok
+
+        //login
+        queryInterface.changeColumn(
+          'Users',
+          'login',
+          { type: Sequelize.STRING, unique: false, allowNull: false },
+          { transaction: t }
+        ),
+
+        //password ok
+
+        // userType
+        queryInterface.removeColumn('Users', 'userType', { transaction: t }),
+
+        //authorized ok
+
+        //createdAt ok
+
+        //updatedAt ok
+
+        //deletedAt ok
+
+        //isActive
+        queryInterface
+          .addColumn(
+            'Users',
+            'isActive',
+            { type: 'INT(1) GENERATED ALWAYS AS (IF(deletedAt IS NULL,  1, NULL)) VIRTUAL' },
+            { transaction: t }
+          )
+          .then(() => {
+            //uniqueKeys
+            return queryInterface.addConstraint(
+              'Users',
+              ['login', 'isActive'],
+              {
+                type: 'unique',
+                name: 'unique_login_isActive'
+              },
+              { transaction: t }
+            )
+          })
+      ])
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(t => {
+      return Promise.all([
+        //id ok
+
+        //login
+        queryInterface.changeColumn(
+          'Users',
+          'login',
+          { type: Sequelize.STRING, unique: true, allowNull: false },
+          { transaction: t }
+        ),
+
+        //password ok
+
+        // userType
+        queryInterface.addColumn(
+          'Users',
+          'userType',
+          { type: Sequelize.ENUM('ufes', 'sead'), allowNull: false, defaultValue: 'sead' },
+          { transaction: t }
+        ),
+
+        //authorized ok
+
+        //createdAt ok
+
+        //updatedAt ok
+
+        //deletedAt ok
+
+        //uniqueKeys
+        queryInterface.removeConstraint('Users', 'unique_login_isActive', { transaction: t }).then(() => {
+          //isActive
+          return queryInterface.removeColumn('Users', 'isActive', { transaction: t })
+        })
+      ])
+    })
+  }
+}
--- db/seeders/20181109154634-users.js
+++ db/seeders/20181109154634-users.js
@@ -11,10 +11,10 @@ let passwordHash = bcrypt.hashSync('senhafraca123', 10)
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-      { id: uuid(), login: "admin@ufes.br", password: passwordHash, userType: 'sead', authorized: true, verifiedAt: new Date() },  
-      { id: uuid(), login: "gerente@ufes.br", password: passwordHash, userType: 'ufes', authorized: true, verifiedAt: new Date() },  
-      { id: uuid(), login: "coordenador@ufes.br", password: passwordHash, userType: 'ufes', authorized: true, verifiedAt: new Date() },  
-      { id: uuid(), login: "comum@ufes.br", password: passwordHash, userType: 'ufes', authorized: true, verifiedAt: new Date() }  
+      { id: uuid(), login: "admin@ufes.br", password: passwordHash, authorized: true, verifiedAt: new Date() },  
+      { id: uuid(), login: "gerente@ufes.br", password: passwordHash, authorized: true, verifiedAt: new Date() },  
+      { id: uuid(), login: "coordenador@ufes.br", password: passwordHash, authorized: true, verifiedAt: new Date() },  
+      { id: uuid(), login: "comum@ufes.br", password: passwordHash, authorized: true, verifiedAt: new Date() }  
     ], {})
   },
 
