9a160a9ba69962ae20fe3f9ec914590a264efff4
annecchini <fernando.void@gmail.com>
2020-04-08 16:17:20 -0300

    PasswordRecover com paranoid

--- app/models/passwordrecover.js
+++ app/models/passwordrecover.js
@@ -1,25 +1,42 @@
+/** @format */
+
 'use strict'
 
-const uuid = require('uuid/v4')
+//Retirei pois estava em beforeCreate
+// const uuid = require('uuid/v4')
 
 module.exports = (sequelize, DataTypes) => {
   const PasswordRecover = sequelize.define(
     'PasswordRecover',
     {
-      token: DataTypes.STRING
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      token: {
+        type: DataTypes.STRING
+      }
     },
-    {
-      paranoid: true
-    }
+    { timestamps: true, paranoid: true }
   )
 
-  PasswordRecover.associate = function(models) {
+  PasswordRecover.associate = function (models) {
     PasswordRecover.belongsTo(models.User, { foreignKey: 'user_id' })
   }
 
-  PasswordRecover.beforeCreate((passwordRecover, _) => {
-    passwordRecover.id = uuid()
-    return passwordRecover
+  //Retirei porque (teoricamente) defaultValue: DataTypes.UUIDV4 cria o valor.
+  // PasswordRecover.beforeCreate((passwordRecover, _) => {
+  //   passwordRecover.id = uuid()
+  //   return passwordRecover
+  // })
+
+  User.beforeDestroy(async (user, _) => {
+    //validação de restrições em modelos relacionados. (onDelete:'RESTRICT')
+    //vazio
+    //operações em modelos relacionados (onDelete:'CASCADE' ou 'SET NULL')
+    //vazio
   })
 
   return PasswordRecover
