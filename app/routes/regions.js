328835b631a2041246a8f80b25bdf460b086080c
annecchini <fernando.void@gmail.com>
2020-05-02 16:28:25 -0300

    regions e restriction agora tem read

--- app/routes/regions.js
+++ app/routes/regions.js
@@ -1,13 +1,17 @@
-module.exports = app => {
-    const api = app.api.regions;
-    const authApi = app.api.auth;
+/** @format */
 
-    app.route(app.get('regionApiRoute'))
-        .post(authApi.authenticationRequired, authApi.adminRequired, api.create)
-        .get(authApi.authenticationRequired, api.list);
-    
-    app.route(app.get('regionApiRoute')+"/:id")
-        .put(authApi.authenticationRequired, authApi.adminRequired, api.update)
-        .delete(authApi.authenticationRequired, authApi.adminRequired, api.delete);
+module.exports = app => {
+  const api = app.api.regions
+  const authApi = app.api.auth
+
+  app
+    .route(app.get('regionApiRoute'))
+    .post(authApi.authenticationRequired, authApi.adminRequired, api.create)
+    .get(authApi.authenticationRequired, api.list)
+
+  app
+    .route(app.get('regionApiRoute') + '/:id')
+    .get(authApi.authenticationRequired, authApi.adminRequired, api.update)
+    .put(authApi.authenticationRequired, authApi.adminRequired, api.update)
+    .delete(authApi.authenticationRequired, authApi.adminRequired, api.delete)
 }
-  
