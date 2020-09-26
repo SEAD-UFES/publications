b1f44c265faec38d974d2f40d16b41556d1f009f
annecchini <fernando.void@gmail.com>
2020-09-26 09:58:37 -0300

    refatoração basica  no modelo RoleType

--- app/models/roletype.js
+++ app/models/roletype.js
@@ -9,40 +9,45 @@ module.exports = (sequelize, DataTypes) => {
   const RoleType = sequelize.define(
     'RoleType',
     {
-      name: DataTypes.STRING,
-      description: DataTypes.STRING,
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
       global: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
+        allowNull: false
       }
     },
-    {}
+    { timestamps: true, paranoid: true }
   )
-  RoleType.associate = function(models) {
-    RoleType.hasMany(models.UserRole, {
-      foreignKey: 'roleType_id'
-    })
 
+  RoleType.associate = function (models) {
+    RoleType.hasMany(models.UserRole, { foreignKey: 'roleType_id' })
     RoleType.belongsToMany(models.Permission, { through: models.RolePermission, foreignKey: 'roleType_id' })
-
-    return RoleType
   }
 
-  RoleType.beforeCreate((roleType, _) => {
-    roleType.id = uuid()
-    return roleType
-  })
-
-  RoleType.prototype.toJSON = function() {
+  RoleType.prototype.toJSON = function () {
     let values = Object.assign({}, this.get())
 
+    //remove fields
+    delete values.deletedAt
+
+    //"follow your nose..."
     values.link = {
       rel: 'roleType',
       href: apiRoutes.find(r => r.key === 'roleTypeApiRoute').value + '/' + values.id
     }
-
-    delete values.createdAt
-    delete values.updatedAt
 
     return values
   }
