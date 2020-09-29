ecb3fe67b65a86ad222ee9e5d184732c0c9c11d0
annecchini <fernando.void@gmail.com>
2020-09-29 18:01:06 -0300

    rebuild Permission model

--- app/models/permission.js
+++ app/models/permission.js
@@ -1,41 +1,50 @@
 /** @format */
 
-const uuid = require('uuid/v4')
 const apiRoutes = require('../../config/apiRoutes.json')
 ;('use strict')
 module.exports = (sequelize, DataTypes) => {
   const Permission = sequelize.define(
     'Permission',
     {
-      name: DataTypes.STRING,
-      description: DataTypes.STRING,
-      target_id: DataTypes.UUID,
-      action_id: DataTypes.UUID
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING
+      },
+      description: {
+        type: DataTypes.STRING
+      },
+      target_id: {
+        type: DataTypes.UUID
+      },
+      action_id: {
+        type: DataTypes.UUID
+      }
     },
-    {}
+    { timestamps: true, paranoid: true }
   )
-  Permission.associate = function(models) {
+
+  Permission.associate = function (models) {
     Permission.belongsTo(models.Target, { foreignKey: 'target_id' })
     Permission.belongsTo(models.Action, { foreignKey: 'action_id' })
     Permission.belongsToMany(models.RoleType, { through: models.RolePermission, foreignKey: 'permission_id' })
-    return Permission
   }
 
-  Permission.beforeCreate((permission, _) => {
-    permission.id = uuid()
-    return permission
-  })
-
-  Permission.prototype.toJSON = function() {
+  Permission.prototype.toJSON = function () {
     let values = Object.assign({}, this.get())
 
+    //remove fields
+    delete values.deletedAt
+
+    //"follow your nose..."
     values.link = {
       rel: 'permission',
       href: apiRoutes.find(r => r.key === 'permissionApiRoute').value + '/' + values.id
     }
-
-    delete values.createdAt
-    delete values.updatedAt
 
     return values
   }
