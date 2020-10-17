274880709365fdd411e774e871b04bfc963d2cbf
annecchini <fernando.void@gmail.com>
2020-10-17 12:16:08 -0300

    aplly unique validation

--- app/validators/vacancy.js
+++ app/validators/vacancy.js
@@ -23,6 +23,9 @@ const validateCallId = async (value, db, mode, item) => {
     const call = await db.Call.findOne({ where: { id: value } })
     if (!call) return 'A chamada não existe.'
   }
+
+  //no errors
+  return null
 }
 
 const validateAssignmentId = async (value, db, mode, item) => {
@@ -41,6 +44,9 @@ const validateAssignmentId = async (value, db, mode, item) => {
     const call = await db.Assignment.findOne({ where: { id: value } })
     if (!call) return 'O cargo não existe.'
   }
+
+  //no errors
+  return null
 }
 
 const validateRegionId = async (value, db, mode, item) => {
@@ -49,6 +55,9 @@ const validateRegionId = async (value, db, mode, item) => {
     const region = await db.Region.findOne({ where: { id: value } })
     if (!region) return 'A Região não existe.'
   }
+
+  //no errors
+  return null
 }
 
 const validateRestrictionId = async (value, db, mode, item) => {
@@ -57,6 +66,9 @@ const validateRestrictionId = async (value, db, mode, item) => {
     const restriction = await db.Restriction.findOne({ where: { id: value } })
     if (!restriction) return 'A Restrição não existe.'
   }
+
+  //no errors
+  return null
 }
 
 const validateQtd = (value, db, mode, item) => {
@@ -74,6 +86,9 @@ const validateQtd = (value, db, mode, item) => {
   if (typeof value !== 'undefined' && !(Number.isInteger(value) || isNumeric(value))) {
     return 'Deve ser um número.'
   }
+
+  //no errors
+  return null
 }
 
 const validateReserve = (value, db, mode, item) => {
@@ -90,6 +105,31 @@ const validateReserve = (value, db, mode, item) => {
   //value is boolean
   if (typeof value !== 'undefined' && value !== true && value !== false && value !== 1 && value !== 0) {
     return 'Deve ser verdadeiro ou falso.'
+  }
+
+  //no errors
+  return null
+}
+
+const validateUniqueVacancy = async (body, db, mode, item, errors) => {
+  if (!errors.call_id && !errors.assignment_id && !errors.region_id && !errors.restriction_id) {
+    // req.params.id está sendo passado em item, o ideal é que o nome fosse mais descritivo, item podem ser qualquer coisa
+    const whereIgnoreOwnId = mode === 'update' ? { id: { [db.Sequelize.Op.not]: item } } : {}
+
+    const call_id = typeof body.call_id !== 'undefined' ? body.call_id : item.call_id
+    const assignment_id = typeof body.assignment_id !== 'undefined' ? body.assignment_id : item.assignment_id
+    const region_id = typeof body.region_id !== 'undefined' ? body.region_id : item ? item.region_id : null
+    const restriction_id =
+      typeof body.restriction_id !== 'undefined' ? body.restriction_id : item ? item.restriction_id : null
+
+    const duplicate = await db.Vacancy.findOne({
+      where: { ...whereIgnoreOwnId, call_id, assignment_id, region_id, restriction_id }
+    })
+
+    if (duplicate) return 'Essa Oferta de vaga já existe.'
+
+    //no errors
+    return null
   }
 }
 
@@ -113,6 +153,9 @@ const validateBody = async (body, db, mode, item) => {
 
   const reserveError = validateReserve(body.reserve, db, mode, item)
   if (reserveError) errors.reserve = reserveError
+
+  const uniqueVacancyError = await validateUniqueVacancy(body, db, mode, item, errors)
+  if (uniqueVacancyError) errors.id = uniqueVacancyError
 
   return !isEmpty(errors) ? errors : null
 }
