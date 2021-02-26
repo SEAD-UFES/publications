57f1b6414ee7abfe74967f6bb6a7918ed64340f0
annecchini <fernando.void@gmail.com>
2021-02-26 17:11:19 -0300

    api.createV2 now is on /v1/register && comment api.create

--- app/api/register.js
+++ app/api/register.js
@@ -8,56 +8,56 @@ module.exports = app => {
   const { validationDevMessage, unknownDevMessage } = require('../helpers/error')
   const { validateBody } = require('../validators/register.js')
 
-  api.create = async (req, res) => {
-    if (!(Object.prototype.toString.call(req.body) === '[object Object]') || !req.body.cpf || !req.body.User.login) {
-      res.status(400).json(error.parse('register-01', {}))
-    } else {
-      req.body.User.authorized = 0
-      models.Person.findOne({
-        where: {
-          cpf: req.body.cpf
-        }
-      }).then(p => {
-        models.User.findOne({
-          where: {
-            login: req.body.User.login
-          }
-        }).then(u => {
-          if (p || u) {
-            let e = {
-              cpf: 'Ok.',
-              login: 'Ok.'
-            }
-            p ? (e.cpf = 'CPF já cadastrado.') : delete e.cpf
-            u ? (e.login = 'Login já cadastrado.') : delete e.login
-            res.status(400).json(error.parse('register-05', e))
-          } else {
-            db.sequelize.transaction(t => {
-              return models.Person.create(req.body, {
-                include: [
-                  {
-                    model: models.User
-                  }
-                ],
-                transaction: t
-              })
-                .then(person => {
-                  res.status(201).json({
-                    id: person.User.id
-                  })
-                })
-                .catch(e => {
-                  t.rollback()
-                  if (e.name === 'SequelizeUniqueConstraintError') res.status(400).json(error.parse('register-02', e))
-                  if (e.name === 'SequelizeValidationError') res.status(400).json(error.parse('register-03', e))
-                  else res.status(500).json(error.parse('register-04', e))
-                })
-            })
-          }
-        })
-      })
-    }
-  }
+  // api.create = async (req, res) => {
+  //   if (!(Object.prototype.toString.call(req.body) === '[object Object]') || !req.body.cpf || !req.body.User.login) {
+  //     res.status(400).json(error.parse('register-01', {}))
+  //   } else {
+  //     req.body.User.authorized = 0
+  //     models.Person.findOne({
+  //       where: {
+  //         cpf: req.body.cpf
+  //       }
+  //     }).then(p => {
+  //       models.User.findOne({
+  //         where: {
+  //           login: req.body.User.login
+  //         }
+  //       }).then(u => {
+  //         if (p || u) {
+  //           let e = {
+  //             cpf: 'Ok.',
+  //             login: 'Ok.'
+  //           }
+  //           p ? (e.cpf = 'CPF já cadastrado.') : delete e.cpf
+  //           u ? (e.login = 'Login já cadastrado.') : delete e.login
+  //           res.status(400).json(error.parse('register-05', e))
+  //         } else {
+  //           db.sequelize.transaction(t => {
+  //             return models.Person.create(req.body, {
+  //               include: [
+  //                 {
+  //                   model: models.User
+  //                 }
+  //               ],
+  //               transaction: t
+  //             })
+  //               .then(person => {
+  //                 res.status(201).json({
+  //                   id: person.User.id
+  //                 })
+  //               })
+  //               .catch(e => {
+  //                 t.rollback()
+  //                 if (e.name === 'SequelizeUniqueConstraintError') res.status(400).json(error.parse('register-02', e))
+  //                 if (e.name === 'SequelizeValidationError') res.status(400).json(error.parse('register-03', e))
+  //                 else res.status(500).json(error.parse('register-04', e))
+  //               })
+  //           })
+  //         }
+  //       })
+  //     })
+  //   }
+  // }
 
   api.createV2 = async (req, res) => {
     const t = await db.sequelize.transaction()
--- app/routes/register.js
+++ app/routes/register.js
@@ -3,6 +3,5 @@
 module.exports = app => {
   const api = app.api.register
 
-  app.route(app.get('register')).post(api.create)
-  app.route('/v1/register-v2').post(api.createV2)
+  app.route(app.get('register')).post(api.createV2)
 }
